88b9815040c419b767186fb0fcf31e0244fde15b
zhangxin <3038119120@qq.com>
2016-01-09 11:34:13 +0800

    android mvp模式做的一个小demo
    android mvp模式做的一个小demo

--- mvp-master/mvp2/src/com/example/mvp2/model/IUserBiz.java
+++
@@ -1,8 +0,0 @@
-package com.example.mvp2.model;
-
-/**
- * login½Ó¿Ú
- * */
-public interface IUserBiz {
-	public void login(String name,String pwd,OnLoginListener loginListener);
-}
--- mvp-master/mvp2/src/com/example/mvp2/model/OnLoginListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.mvp2.model;
-
-/**
- * login成功失败监听接口
- * */
-public interface OnLoginListener {
-	void loginSuccess(UserInfo userInfo);
-	void loginFail(String msg);
-}
